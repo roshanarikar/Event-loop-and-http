e41e5622c9483e5b1570a7ebe207b9948017675c
Roshan Arikar <roshan.g.arikar@gmail.com>
2022-03-03 23:48:55 +0530

    hello commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express= require("express");
+
+const app = express();
+
+// app.get("/users",function (req,res){
+//     console.log("Hello");
+// });
+
+app.listen(5000,()=>{
+    console.log("Hello");
+});
