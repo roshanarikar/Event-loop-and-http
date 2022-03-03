cc159093beda657bf1116bf6f4b973bff809f09b
Roshan Arikar <roshan.g.arikar@gmail.com>
2022-03-04 00:13:28 +0530

    index add

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express= require("express");
 
 const app = express();
 
-// app.get("/users",function (req,res){
-//     console.log("Hello");
-// });
+app.get("/users",function (req,res){
+    console.log("Hello");
+});
 
 app.listen(5000,()=>{
-    console.log("Hello");
+    console.log("Listening on port 5000");
 });
